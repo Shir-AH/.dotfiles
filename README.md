e33c5c8c7d17b2d2b78cdd9dbecf28962dd4ca4a
Shir-AH <62842368+Shir-AH@users.noreply.github.com>
2024-02-07 11:42:04 +0200

    initial commit
    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # .dotfiles
+
+## Initial Setup
+
+```powershell
+cd $HOME
+winget install Git.Git
+git clone https://github.com/Shir-AH/.dotfiles.git
+.\.dotfiles\setup.ps1
+```
+
+clone Obsidian vaults into installation location
+log in to chrome and sync settings
+log in to vscode and sync settings
+log in to steam
+log in to spotify
+log in to discord
+log in to minecraft
---
+++ setup.ps1
@@ -0,0 +1,20 @@
+$DOTFILES = "$HOME\.dotfiles"
+New-Item -Path $HOME\Repositories -ItemType Directory -Force
+
+winget import .\winget\winget.export
+New-Item -Path $HOME\.gitconfig -ItemType SymbolicLink -Value .\git\.gitconfig
+New-Item -Path $HOME\AppData\Local\Microsoft\PowerToys -ItemType SymbolicLink -Value .\PowerToys
+wsl --install -d Ubuntu
+
+
+# minecraft setup
+$MINECRAFT = "$HOME\AppData\Roaming\.minecraft\"
+New-Item -Path $MINECRAFT -ItemType Directory -Force
+New-Item -Path $MINECRAFT\saves -ItemType Directory -Force
+New-Item -Path $MINECRAFT\resourcepacks -ItemType Directory -Force
+New-Item -Path $MINECRAFT -ItemType File -Name "allowed_symlinks.txt"
+Write-Output "[prefix]$DOTFILES\minecraft\" > $MINECRAFT\allowed_symlinks.txt
+New-Item -Path $MINECRAFT\resourcepacks -ItemType SymbolicLink -Value $DOTFILES\minecraft\resourcepacks
+Get-ChildItem $DOTFILES\minecraft\saves | Foreach-Object { 
+    New-Item -Path $MINECRAFT\saves\$_.Name -ItemType SymbolicLink -Value $_.FullName
+}
